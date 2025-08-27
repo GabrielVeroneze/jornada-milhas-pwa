dde314014167d8f2fcbd32ab25825a0a138add30
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-08-27 20:58:50 -0300

    🔧 chore: configura o plugin `vite-plugin-pwa`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
