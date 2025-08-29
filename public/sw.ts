9c70828667dc7cd4e425773acca5f3dcf6d0e63c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-08-29 17:17:09 -0300

    ✨ feat: implementa estratégia cache-first no service worker
    Adiciona uma abordagem de cache-first ao evento fetch no service worker.

--- public/sw.ts
+++ public/sw.ts
@@ -16,4 +16,26 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     console.log('Interceptando requisição', event.request.url)
+
+    event.respondWith(cachePrimeiro(event.request))
 })
+
+const cachePrimeiro = async (request) => {
+    const respostaDoCache = await caches.match(request)
+
+    if (respostaDoCache) {
+        return respostaDoCache
+    }
+
+    const respostaRede = await fetch(request)
+
+    atualizaCache(request, respostaRede.clone())
+
+    return respostaRede
+}
+
+const atualizaCache = async (request, response) => {
+    const cache = await caches.open(CACHE_NAME)
+
+    await cache.put(request, response)
+}
