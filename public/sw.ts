07bf03c8a62ecacde61efc3fb67f4d9ef807121a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-08-30 17:36:49 -0300

    ♻️ refactor: refatora o service worker para usar o Workbox
    Substitui a lógica de cache personalizada do service worker pelos módulos do Workbox para pré-cache, limpeza de cache e cache de imagens, usando a estratégia `StaleWhileRevalidate`.

--- public/sw.ts
+++ public/sw.ts
@@ -1,69 +1,30 @@
-const CACHE_NAME = 'meu_cache'
+import { clientsClaim } from 'workbox-core'
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
+import { ExpirationPlugin } from 'workbox-expiration'
+import { Route } from 'workbox-routing'
+import { StaleWhileRevalidate } from 'workbox-strategies'
 
-self.addEventListener('install', (event) => {
-    console.log('Instalando o service worker', event)
+self.skipWaiting()
+clientsClaim()
 
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(['/index.html']).then(() => {
-                    self.skipWaiting()
-                })
-            })
-    )
-})
+cleanupOutdatedCaches()
+precacheAndRoute(self.__WB_MANIFEST)
 
-self.addEventListener('fetch', (event) => {
-    console.log('Interceptando requisição', event.request.url)
-
-    event.respondWith(cacheEnquantoAtualiza(event.request))
-})
-
-const cacheEnquantoAtualiza = async (request) => {
-    const cache = await caches.open(CACHE_NAME)
-
-    const respostaEmCache = await cache.match(request)
-
-    const respostaDaRede = fetch(request).then(resposta => {
-        cache.put(request, resposta.clone())
-
-        return resposta
+const imageRoute = new Route(
+    (request) => {
+        return request.destination === 'image'
+    },
+    new StaleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
     })
-
-    return respostaEmCache || respostaDaRede
-}
-
-const cachePrimeiro = async (request) => {
-    const respostaDoCache = await caches.match(request)
-
-    if (respostaDoCache) {
-        return respostaDoCache
-    }
-
-    const respostaRede = await fetch(request)
-
-    atualizaCache(request, respostaRede.clone())
-
-    return respostaRede
-}
-
-const redePrimeiro = async (request) => {
-    const respostaDaRede = await fetch(request)
-
-    if (respostaDaRede) {
-        atualizaCache(request, respostaDaRede.clone())
-
-        return respostaDaRede
-    }
-
-    const respostaCache = await caches.match(request)
-
-    return respostaCache
-}
-
-const atualizaCache = async (request, response) => {
-    const cache = await caches.open(CACHE_NAME)
-
-    await cache.put(request, response)
-}
+)
